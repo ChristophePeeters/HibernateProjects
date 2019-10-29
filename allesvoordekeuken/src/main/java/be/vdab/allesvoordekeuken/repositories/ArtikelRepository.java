d5c5d0cfbeb4577d791f8227cb365a0b5d475a8c
christophepeeters <Kettinkje1>
2019-10-29 09:00:45 +0100

    Delete keuken

--- allesvoordekeuken/src/main/java/be/vdab/allesvoordekeuken/repositories/ArtikelRepository.java
+++
@@ -1,9 +0,0 @@
-package be.vdab.allesvoordekeuken.repositories;
-
-import be.vdab.allesvoordekeuken.domain.Artikel;
-
-import java.util.Optional;
-
-public interface ArtikelRepository {
-    Optional<Artikel> findById(long id);
-}
--- allesvoordekeuken/src/test/resources/insertArtikel.sql
+++
@@ -1,1 +0,0 @@
-insert into artikels(naam,aankoopprijs,verkoopprijs) values('test',100,120);
